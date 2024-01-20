a9506fc882dd1d5598f4b4d575d4f845fd7d19c2
Fernando Ocaña <42670787+z0r3f@users.noreply.github.com>
2024-01-20 18:47:31 +0100

    feat: able to answer without command is requested

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
+use std::error::Error;
 use std::time::Duration;
 use log::{error, info, LevelFilter};
 use pretty_env_logger::formatted_builder;
 use teloxide::{prelude::*, utils::command::BotCommands};
-use teloxide::types::ParseMode;
+use teloxide::types::{Me, ParseMode};
 use tokio::time;
 use telegram::Telegram;
 
@@ -35,7 +36,13 @@ async fn main() {
             }
         }
     }
-    Command::repl(bot, answer).await;
+
+    let handler = dptree::entry()
+        .branch(Update::filter_message().endpoint(message_handler));
+    // .branch(Update::filter_callback_query().endpoint(callback_handler))
+    // .branch(Update::filter_inline_query().endpoint(inline_query_handler));
+
+    Dispatcher::builder(bot, handler).enable_ctrlc_handler().build().dispatch().await;
 }
 
 #[derive(BotCommands, Clone, Debug)]
@@ -47,13 +54,30 @@ enum Command {
     Info(String),
 }
 
-async fn answer(bot: Bot, msg: Message, cmd: Command) -> ResponseResult<()> {
-    info!("Received message: {:?}", msg);
-    info!("Parsed command: {:?}", cmd);
-    match cmd {
-        Command::Help => bot.send_message(msg.chat.id, Command::descriptions().to_string()).await?,
-        Command::Info(word) => bot.send_message(msg.chat.id, build_info_response(word.as_str()).await).parse_mode(ParseMode::MarkdownV2).await?,
-    };
+async fn message_handler(
+    bot: Bot,
+    msg: Message,
+    me: Me,
+) -> Result<(), Box<dyn Error + Send + Sync>> {
+    if let Some(text) = msg.text() {
+        info!("Received message: {:?}", msg);
+        match BotCommands::parse(text, me.username()) {
+            Ok(Command::Help) => {
+                bot.send_message(msg.chat.id, Command::descriptions().to_string()).await?;
+            }
+            Ok(Command::Info(text)) => {
+                bot.send_message(msg.chat.id, build_info_response(&text).await).parse_mode(ParseMode::MarkdownV2).await?;
+            }
+            Err(_) => {
+                if text.starts_with('/') {
+                    bot.send_message(msg.chat.id, "Command not found!").await?;
+                } else {
+                    bot.send_message(msg.chat.id, build_info_response(&text).await).parse_mode(ParseMode::MarkdownV2).await?;
+                }
+            }
+        }
+    }
+
     Ok(())
 }
 
